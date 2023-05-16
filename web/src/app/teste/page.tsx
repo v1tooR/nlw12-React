60b18f0620f7d7dc0f00891d42d27c339738ae0c
v1tooR <victorssantos572@gmail.com>
2023-05-15 23:14:00 -0300

    Aula 1 - Frontend

---
+++ web/src/app/teste/page.tsx
@@ -0,0 +1,3 @@
+export default function Teste(){ // Cria rotas dentro do URL a partir de pastas
+  return <h1>Teste</h1> // Toda page.tsx é uma pagina web
+}
---
+++ web/src/components/Button.tsx
@@ -0,0 +1,9 @@
+const styles = {
+  color: '#f00', //objeto que aplica cores
+}
+
+export function Button(){ //componente
+  return(
+    <p style={styles}>botao :p</p>
+  )
+}
