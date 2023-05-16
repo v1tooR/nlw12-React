33db014b2e145367f3e5934915c49ea5dcedf5f7
v1tooR <victorssantos572@gmail.com>
2023-05-15 23:39:13 -0300

    mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+//npm run start
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello world</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color:'#FFF',
+    fontWeight: '700',
+    fontSize: 50,
+  },
+});
