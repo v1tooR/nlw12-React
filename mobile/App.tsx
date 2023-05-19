c991455c5c6d6faf109066746ca50eb8ee3bc3b9
v1tooR <victorssantos572@gmail.com>
2023-05-18 23:17:19 -0300

    AULA 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,12 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+  } from '@expo-google-fonts/roboto'
+
+import {
+    BaiJamjuree_700Bold,
+  } from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from './src/assets/bg-blur.png'
 //npm run start
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  }); // Um objeto que vai armazenar as informações de cada uma das fonts
+
+  if(!hasLoadedFonts){
+    return null
+  }
+
   return (                               
-    <View className='bg-zinc-50 flex-1 items-center justify-center'>
-      <Text className='text-zinc-950 font-bold text-5xl'>Olá mundo!</Text>
+    <ImageBackground 
+      source={blurBg} 
+      className='bg-gray-900 flex-1 items-center relative'
+      imageStyle={{position: 'absolute', left: '-100%'}}
+    > 
       <StatusBar style="light" translucent />
-    </View>
+    </ImageBackground>
   );
-}
+}{/** propriedade que permite inserir imagens dentro do display */}
 
---
+++ mobile/src/assets/assets.d.ts
@@ -0,0 +1,3 @@
+declare module '*.png'
+
+//declara apenas em script TS a permissão de todo aquivo .png
