ce6c6f7d5fbbc80cc3b0c62d51ed011d16691582
v1tooR <victorssantos572@gmail.com>
2023-05-25 00:02:03 -0300

    AULA 2 MOBILE

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground } from 'react-native';
+import { ImageBackground, View, Text, TouchableOpacity } from 'react-native';
+import { styled } from 'nativewind';
 import {
   useFonts,
   Roboto_400Regular,
@@ -11,6 +12,10 @@ import {
   } from '@expo-google-fonts/bai-jamjuree'
 
 import blurBg from './src/assets/bg-blur.png'
+import Stripes from './src/assets/stripes.svg'
+import NLWLogo from './src/assets/nlw-spacetime-logo.svg'
+//o pacote SVG transform faz com que o a imagem SVG se transforme em um component
+const StyledStripes = styled(Stripes) //recebe um componente
 //npm run start
 export default function App() {
   const [hasLoadedFonts] = useFonts({
@@ -26,9 +31,33 @@ export default function App() {
   return (                               
     <ImageBackground 
       source={blurBg} 
-      className='bg-gray-900 flex-1 items-center relative'
+      className='bg-gray-900 px-8 py-10 flex-1 items-center relative'
       imageStyle={{position: 'absolute', left: '-100%'}}
     > 
+      <StyledStripes className='absolute left-2'/>
+
+      <View className='flex-1 items-center justify-center gap-6'>
+        <NLWLogo/>
+        <View className='space-y-2'>
+          <Text className='text-2xl font-title text-center leading-tight text-gray-50'>
+            Sua cápsula do tempo
+          </Text>
+          <Text className='text-center font-body text-base leading-relaxed text-gray-100'>
+            Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+          </Text>
+        </View>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className='rounded-full bg-green-500 px-5 py-2'
+        >
+          <Text className='font-alt text-sm uppercase text-black'>Cadastrar lembrança</Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className='text-center font-body text-sm leading-relaxed text-gray-200 '>
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+
       <StatusBar style="light" translucent />
     </ImageBackground>
   );
--- mobile/src/assets/assets.d.ts
+++ mobile/src/assets/assets.d.ts
@@ -1,3 +1,8 @@
 declare module '*.png'
-
+declare module "*.svg" {
+  import React from 'react';
+  import { SvgProps } from "react-native-svg";
+  const content: React.FC<SvgProps>;
+  export default content;
+}
 //declara apenas em script TS a permissão de todo aquivo .png
